9b330d586c11f42fd8f07507e311a29fdf1f34f5
Arina Shmodina <ApelsinGilka@yandex.ru>
2026-04-20 19:24:44 +0300

    clear

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-// import * as fs from "fs";
-// import * as readline from readline
-// import fs from "fs";
-// import readline from "readline";
-
 import showMenu from "./modules/interface.js";
-
-// const pathToParticipants = "./data/participants.json";
-// const pathToHistory = "./data/history.json";
-
-// const participants = JSON.parse(fs.readFileSync(pathToParticipants, "utf-8"));
-// const history = JSON.parse(fs.readFileSync(pathToHistory, "utf-8"));
 
 showMenu();
